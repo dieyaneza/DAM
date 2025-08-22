a4aa09f807b47c506619a0d4c2c22f87fa896e43
dieyaneza <die.yaneza@duocuc.cl>
2025-08-22 15:06:17 -0400

    Clase2: primer comit

---
+++ Clase2.kt
@@ -0,0 +1,20 @@
+fun main(){
+
+    println("Inicio de programa")
+
+    val sum = 5+3
+    val res = 5-3
+    val mul = 5*3
+    val div = 5/3
+    val mod = 5 % 3
+
+    println("sum: $sum")
+    println("res: $res")
+    println("mul: $mul")
+    println("div: $div")
+    println("mod: $mod")
+
+
+
+
+}
